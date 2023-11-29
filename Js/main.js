320e00841446b3f506f1f437e5a8c8ba4890408a
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-11-29 21:09:33 +0100

    add final price with the saleYoung and saleSenior

--- Js/main.js
+++ Js/main.js
@@ -10,48 +10,29 @@ const priceKm = 0.21;
 
 // add Event Listener
 generator.addEventListener('click' , function() {
-console.log('clicked', chilometers.value , age.value);
+//console.log('clicked', chilometers.value , age.value);
 
-    const km = chilometers.value;
-    const age_user = age.value;
-    console.log(km , age_user);
-    console.log(priceKm * km);
+const km = chilometers.value;
+const age_user = age.value;
+//console.log(km , age_user);
+
+const price = km * priceKm;
+console.log(price);
+
+if (age_user < 18) {
+    //  sconto per i minorenni
+const saleYoung = (price * 20) / 100;
+const priceYoung = price - saleYoung;
+console.log(priceYoung.toFixed(2));
+
+} else if (age_user > 65) {
+//      sconto per gli over 65
+const saleSenior = (price * 40) / 100;
+const priceSenior = price - saleSenior;
+console.log(priceSenior.toFixed(2));
+
+// Calcolo del prezzo del biglietto tra i 18 e i 65 anni
+} else {
+console.log(price.toFixed(2));
+}
 })
-
-
-
-// // sconto per i minorenni
-// const saleYoung = (price * 20) / 100;
-// const priceYoung = price - saleYoung;
-
-// // sconto per gli over 65
-// const saleSenior = (price * 40) / 100;
-// const priceSenior = price - saleSenior;
-
-
-
-
-// // Calcolo del prezzo del biglietto scontato per i minorenni
-//  if (age < 18) {
-    
-//     console.log(priceYoung.toFixed(2));
-//     document.getElementById('total_price').innerHTML = priceYoung.toFixed(2);
-   
-
-    
-// // Calcolo del prezzo del biglietto scontato per gli over 65
-//  } else if (age > 65) {
-    
-//      console.log(priceSenior.toFixed(2));
-//      document.getElementById('total_price').innerHTML = priceSenior.toFixed(2);
-
-// //  Calcolo del prezzo del biglietto tra i 18 e i 65 anni
-//  } else {
-    
-//      console.log(price.toFixed(2));
-//      document.getElementById('total_price').innerHTML = price.toFixed(2);
-//  }
-
-
- 
-
