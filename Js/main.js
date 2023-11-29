1058aafde9459023dfa8fc97055109816169dfa8
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-11-29 18:19:45 +0100

    add price of the ticket with km and age

--- Js/main.js
+++ Js/main.js
@@ -1,16 +1,24 @@
+// DOM elements
 document.getElementById('fullname');
 const chilometers = document.getElementById('chilometers');
 const age = document.getElementById('age');
 
 const generator = document.getElementById('generator');
 
+//  costo del biglietto
+const priceKm = 0.21;
+
+// add Event Listener
 generator.addEventListener('click' , function() {
-    console.log('clicked', chilometers.value , age.value);
+console.log('clicked', chilometers.value , age.value);
+
+    const km = chilometers.value;
+    const age_user = age.value;
+    console.log(km , age_user);
+    console.log(priceKm * km);
 })
 
-// // costo del biglietto
-// const priceKm = 0.21;
-// const price = priceKm * chilometers;
+
 
 // // sconto per i minorenni
 // const saleYoung = (price * 20) / 100;
